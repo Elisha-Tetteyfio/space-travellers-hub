6121a0551e6edbd7181fedd940055027543b5563
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-08-03 23:11:47 +0000

    Create table for elements

--- src/missions/Mission.js
+++ src/missions/Mission.js
@@ -3,12 +3,12 @@ import React from 'react';
 /* eslint-disable react/prop-types */
 function Mission({ name, id, description }) {
   return (
-    <li id={id}>
-      <div>{name}</div>
-      <div>{description}</div>
-      <div><button type="button">Status</button></div>
-      <div><button type="button">Join Mission</button></div>
-    </li>
+    <tr id={id}>
+      <td>{name}</td>
+      <td>{description}</td>
+      <td><button type="button">Status</button></td>
+      <td><button type="button">Join Mission</button></td>
+    </tr>
   );
 }
 
